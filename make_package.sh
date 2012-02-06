eaddef65fafa18862b5fa6f287fd068d22b192f6
Ira Cooke <iracooke@gmail.com>
2012-02-06 15:37:27 +1100

    Changed omssa.xml so it supports in-built databases and fasta files from a user's history

--- make_package.sh
+++ make_package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm protk-toolshed.tar.bz2
-tar --exclude=.hg/ --exclude=tool-data/datatypes_conf.xml --exclude=lib/ --exclude=display_applications --exclude=protk-toolshed.tar -cvf  protk-toolshed.tar ./
+tar --exclude=.hg/ --exclude=tool-data/datatypes_conf.xml --exclude=lib/ --exclude=display_applications --exclude=protk-toolshed.tar --exclude=make_package.sh --exclude=README.md -cvf  protk-toolshed.tar ./
 bzip2 protk-toolshed.tar
